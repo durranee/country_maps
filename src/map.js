07e9b36dfed26ac8c5c43b55704182ad6df52f94
durranee <durranee@hotmail.com>
2018-06-04 15:39:22 +0100

    connected to restcountries api to retrieve data

--- src/map.js
+++ src/map.js
@@ -2,10 +2,17 @@ var basic = new Datamap({
   element: document.getElementById("container"),
     done: function(datamap) {
             datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
-                alert(geography.properties.name);
+              country_name = geography.properties.name;
+                details = getData(country_name);
+                console.log(details);
             });
         }
 });
-console.log(basic);
-// basic.options.fills.defaultFill = "#000"
-console.log(basic.options.fills.defaultFill);
+
+function getData(countru_name) {
+  api_link = `https://restcountries.eu/rest/v2/name/${country_name}?fields=name;capital;currencies;flag;population`
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", api_link, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText);
+}
