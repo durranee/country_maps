1de01c8563790f6e5ed0d90c7feb42a9479f1095
durranee <durranee@hotmail.com>
2018-06-04 17:22:56 +0100

    connected to restcountries api and updated page on click

--- src/map.js
+++ src/map.js
@@ -1,18 +1,33 @@
 var basic = new Datamap({
   element: document.getElementById("container"),
-    done: function(datamap) {
-            datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
-              country_name = geography.properties.name;
-                details = getData(country_name);
-                console.log(details);
-            });
-        }
+  done: function(datamap) {
+    datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+      country_id = geography.id;
+      details = getData(country_id)
+        .then(function(details) {
+          console.log(details);
+          updatePage(details);
+        });
+    });
+  }
 });
 
-function getData(countru_name) {
-  api_link = `https://restcountries.eu/rest/v2/name/${country_name}?fields=name;capital;currencies;flag;population`
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", api_link, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return JSON.parse(xmlHttp.responseText);
+function getData(country_id) {
+  api_link = `https://restcountries.eu/rest/v2/alpha/${country_id}`
+  data = fetch(api_link)
+    .then(function(response) {
+      return response.json();
+    })
+    .catch(function(error) {
+      alert(error);
+    });
+  return data;
+}
+
+function updatePage(details) {
+  document.getElementById("name").innerHTML = details.name;
+  document.getElementById("capital").innerHTML = details.capital;
+  document.getElementById("currency").innerHTML = details.currencies[0].name;
+  document.getElementById("population").innerHTML = details.population;
+  document.getElementById("flag").innerHTML = details.flag;
 }
